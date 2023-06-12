40f816d426b0362f9556595c75effd92ba1c004b
centiago_001 <55315949+centiago001@users.noreply.github.com>
2023-06-12 00:08:10 +0000

    devloped the function

--- subarray-with-given-sum.py
+++ subarray-with-given-sum.py
@@ -1,1 +1,12 @@
-
+def specific_sum(lst,result):
+    sum_var=0
+    start=0
+    for i in range (0,len(lst)):
+        if sum_var ==result:
+            print((start+1),(i))
+            break
+        elif sum_var<result:
+            sum_var=sum_var+lst[i]
+        elif sum_var>result:
+            sum_var=sum_var-lst[start]
+            start=start+1
